8913bff5b3d909c94f5eeffdc5b1139b102ebce9
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-07-09 04:20:42 +0530

    Re Order Files

--- game/__main__.py
+++ game/__main__.py
@@ -1,8 +1,12 @@
+from game.directing.scene_manager import SceneManager
+from game.directing.director import Director
 #More Code TBU  
+
+
 def main():
-    #director = Director(SceneManager.VIDEO_SERVICE)
-    #director.start_game()
-    director = director #Dont run without removing this line
+    director = Director(SceneManager.VIDEO_SERVICE)
+    director.start_game()
+    #director = director #Dont run without removing this line
 
 if __name__ == "__main__":
     main()
