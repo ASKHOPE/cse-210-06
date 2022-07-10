c2d0a4eecc64f7e696d4b00aba65025228122833
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-07-10 23:53:44 +0530

    Reorganise

--- portalrush/game/constants.py
+++ portalrush/game/constants.py
@@ -11,14 +11,14 @@ CENTER_X = SCREEN_WIDTH / 2
 CENTER_Y = SCREEN_HEIGHT / 2
 
 # FONT
-FONT_FILE = "cse-210-06/assets/fonts/zorque.otf"
+FONT_FILE = "cse-210-06/portalrush/assets/fonts/zorque.otf"
 FONT_SMALL = 32
 FONT_LARGE = 48
 
 # SOUND
-COLLIDE_SOUND = "cse-210-06/assets/sounds/boing.wav"
-WELCOME_SOUND = "cse-210-06/assets/sounds/start.wav"
-OVER_SOUND = "cse-210-06/assets/sounds/over.wav"
+COLLIDE_SOUND = "cse-210-06/portalrush/assets/sounds/boing.wav"
+WELCOME_SOUND = "cse-210-06/portalrush/assets/sounds/start.wav"
+OVER_SOUND = "cse-210-06/portalrush/assets/sounds/over.wav"
 
 
 # TEXT
@@ -82,14 +82,14 @@ SCORE_FORMAT = "SCORE: {}"
 
 # Player (Espiritu)
 ESPIRITU_GROUP = "espiritu"
-ESPIRITU_IMAGE = "cse-210-06/assets/images/player.png"
+ESPIRITU_IMAGE = "cse-210-06/portalrush/assets/images/player.png"
 ESPIRITU_WIDTH = 28
 ESPIRITU_HEIGHT = 28
 ESPIRITU_VELOCITY = 6
 
 # Chaser (Reaper)
 REAPER_GROUP = "reaper"
-REAPER_IMAGES = "cse-210-06/assets/images/reaper.png"
+REAPER_IMAGES = "cse-210-06/portalrush/assets/images/reaper.png"
 REAPER_WIDTH = 106
 REAPER_HEIGHT = 28
 REAPER_RATE = 6
