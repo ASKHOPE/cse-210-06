cbfbe59c507de132fa300583caa53ec2219b7967
Marcus Blanc <90995556+Blancmarcus2@users.noreply.github.com>
2022-07-14 00:06:05 -0400

    Update raylib_mouse_service.py

--- portalrush/game/services/raylib/raylib_mouse_service.py
+++ portalrush/game/services/raylib/raylib_mouse_service.py
@@ -13,26 +13,56 @@ class RaylibMouseService(MouseService):
         self._buttons["right"] = pyray.MOUSE_BUTTON_RIGHT
     
     def get_coordinates(self):
+        """get the position coordinate when moving
+        
+        Args:
+            Self.
+        """
         x = pyray.get_mouse_x()
         y = pyray.get_mouse_y()
         return Point(x, y)
 
     def has_mouse_moved(self):
+        """Define the key control
+        
+        Args:
+            key control of move and position.
+        """
         mouse_delta = pyray.get_mouse_delta()
         return mouse_delta.x > 0 or mouse_delta.y > 0
     
     def is_button_down(self, button):
+        """Define the key control
+        
+        Args:
+            key control down with the raylib.
+        """
         raylib_button = self._buttons[button]
         return pyray.is_mouse_button_down(raylib_button)
     
     def is_button_pressed(self, button):
+        """Define the key control
+        
+        Args:
+            key control pressed with raylib.
+        """
         raylib_button = self._buttons[button]
         return pyray.is_mouse_button_pressed(raylib_button)
         
     def is_button_released(self, button):
+        """Define the key control
+        
+        Args:
+            key control release with raylib.
+        """
         raylib_button = self._buttons[button]
         return pyray.is_mouse_button_released(raylib_button)
     
     def is_button_up(self, button):
+        """Define the key control
+        
+        Args:
+            key control up with raylib.
+        """
         raylib_button = self._buttons[button]
         return pyray.is_mouse_button_up(raylib_button)
