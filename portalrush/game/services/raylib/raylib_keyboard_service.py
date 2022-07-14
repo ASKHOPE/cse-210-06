5164efb557816dcd9c3ec1aba4297ceb0f15506e
Marcus Blanc <90995556+Blancmarcus2@users.noreply.github.com>
2022-07-14 00:01:42 -0400

    Update raylib_keyboard_service.py

--- portalrush/game/services/raylib/raylib_keyboard_service.py
+++ portalrush/game/services/raylib/raylib_keyboard_service.py
@@ -21,17 +21,37 @@ class RaylibKeyboardService(KeyboardService):
         self._keys["space"] = pyray.KEY_SPACE
 
     def is_key_down(self, key):
+        """Define the key control
+        
+        Args:
+            key control of the down move and position.
+        """
         raylib_key = self._keys[key.lower()]
         return pyray.is_key_down(raylib_key)
     
     def is_key_pressed(self, key):
+        """Define the key control
+        
+        Args:
+            key control of when pressing the keyboard.
+        """
         raylib_key = self._keys[key.lower()]
         return pyray.is_key_pressed(raylib_key)
     
     def is_key_released(self, key):
+        """Define the key control
+        
+        Args:
+            key control of the release move.
+        """
         raylib_key = self._keys[key.lower()]
         return pyray.is_key_released(raylib_key)
     
     def is_key_up(self, key):
+        """Define the key control
+        
+        Args:
+            key control of the up move and position.
+        """
         raylib_key = self._keys[key.lower()]
         return pyray.is_key_up(raylib_key)
